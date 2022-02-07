2bd35ca31fb8b0c20a1738bebd66ae9e67f83a4f
Muhammad Noman <e4coder@gmail.com>
2022-02-07 19:37:27 +0500

    security issue

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDyV6hMn0VB5nL8ALAAP6XeT1XeHy1FqMU",
+    apiKey: "AIzaSyDyV6hMn0VB5nL8ALA1FqMU",
     authDomain: "ulu-twitter.firebaseapp.com",
     projectId: "ulu-twitter",
     storageBucket: "ulu-twitter.appspot.com",
     messagingSenderId: "975303638113",
-    appId: "1:975303638113:web:3fdebccf83cded0a0fbe20",
+    appId: "b:3fdef83cded0a0fbe20",
     measurementId: "G-XP0XR7ZD01"
 };
 
